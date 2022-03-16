51c0e6f4079661aa9f493510ab9047db9d2e385d
Kane Mathis <admin@kanemathis.com>
2022-03-16 17:50:10 -0600

    commit tree 4b1ceeee16982f458c69f22dfbe24c511a5aaa53

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+ return ('This one is on me!')}
+ if (distance > 2000 && distance < 2500) {
+   return ('I will gladly take your thirty bucks.')
+ }
+ if (distance > 2500){
+   return ('No can do.')
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response = city === "NYC" ?  "Ok, sounds good." : "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip === 'generous'){
+  return ('Thank you so much.');
+}
+else if (tip === 'not as generous'){
+  return ('Thank you.');
+}
+else {
+return ('Bye.');
+}
 }
